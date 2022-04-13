02de55da9ffc61733fda2db4ec7d593fc54c94d7
Zortos <65777760+zortos293@users.noreply.github.com>
2022-04-13 22:35:32 +0200

    Update ProxyBoot.sh

--- ProxyBoot.sh
+++ ProxyBoot.sh
@@ -1,15 +1,15 @@
 #/bin/sh
 # wget "https://Zortosdev.tech/ProxyBoot.sh" && chmod +x ./ProxyBoot.sh && ./ProxyBoot.sh
 cd /root/
-mkdir /root/ZortosProxy
+sudo mkdir /root/ZortosProxy
 cd /root/ZortosProxy/
-apt update
-apt install python3 -y
-apt install python3-pip -y
+sudo apt update
+sudo apt install python3 -y
+sudo apt install python3-pip -y
 curl -o /root/ZortosProxy/pysoxy.py  "https://raw.githubusercontent.com/zortos293/ZortosCDN/master/pysoxy.py"
 curl -o /root/ZortosProxy/requirements.txt "https://raw.githubusercontent.com/zortos293/ZortosCDN/master/requirements.txt"
-ufw enable 
-ufw allow 5000/tcp
-ufw allow 5000/udp
+sudo ufw enable 
+sudo ufw allow 5000/tcp
+sudo ufw allow 5000/udp
 pip install -r /root/ZortosProxy/requirements.txt
 nohup python3 ./root/ZortosProxy/pysoxy.py & 
